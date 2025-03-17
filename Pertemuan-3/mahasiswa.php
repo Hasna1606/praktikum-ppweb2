ad47aac2690a20820d5177fc6bb3cdef5c13fe64
Hasna1606 <hasnaghaniyyah97@gmail.com>
2025-03-17 12:09:06 +0700

    pertemuan3

---
+++ Pertemuan-3/mahasiswa.php
@@ -0,0 +1,16 @@
+<?php
+
+class mahasiswa {
+    public $nama;
+    public $nama;
+    public $nama;
+}
+
+$siti = new Mahasiswa();
+$siti->nama = "Siti Munaroh"
+
+$tono = new Mahasiswa();
+$tono->nama = "Tono Suhartono"
+
+echo $siti->nama . "<br>";
+echo $tono->nama;
